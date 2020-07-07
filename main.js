eb1bfc7a5d4ba57ead87fcf7d56903e5f066a5f6
Ricardo <ricardopuatoiii@gmail.com>
2020-07-07 11:59:54 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const inputField = document.querySelector("#input");
 const videoList = document.querySelector("#videos");
 const playAllBtn = document.querySelector("#playAll");
 const pauseAllBtn = document.querySelector("#pauseAll");
-const buttonField = document.querySelector("#buttons");
+const removeAllBtn = document.querySelector("#removeAll");
 
 inputField.addEventListener("keydown", e => {
     if(e.keyCode === 13){
         //Check if correct input.
-        videoCode = inputField.value.split("=").pop().toString();
+        videoCode = inputField.value.replace("&","=").split("=").slice(1,2).toString();
         videoCode = "https://www.youtube.com/embed/" + videoCode + "?&mute=1&enablejsapi=1";
         videoList.innerHTML += `<iframe class="video" width="640" height="360" src="` + videoCode + `"allowscriptaccess="always" frameborder="0" allowfullscreen></iframe>`
     }
@@ -24,4 +24,8 @@ pauseAllBtn.addEventListener("click", e =>{
     for (let index = 0; document.querySelectorAll("iframe")[index] != undefined ; index++) {
         document.querySelectorAll("iframe")[index].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
     }
+})
+
+removeAllBtn.addEventListener("click", e => {
+    videoList.innerHTML = "";
 })
